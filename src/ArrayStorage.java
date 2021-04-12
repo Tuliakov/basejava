cf6dfdb22c2f75bf9d585c529811e3cd1c261c18
TuliakovM <tuliakovm@gmail.com>
2021-04-12 19:49:25 +0300

    add logic for methods

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,84 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    Resume[] storage = new Resume[10];
 
     void clear() {
+        Arrays.fill(storage, null);
     }
 
     void save(Resume r) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
+        for (Resume resume : storage) {
+            if (resume.uuid.equals(uuid)) {
+                return resume;
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null && storage[i].uuid != null && storage[i].uuid.equals(uuid)) {
+                storage[i] = null;
+            }
+        }
+        removeNulls(storage);
+    }
+
+    private static void removeNulls(Resume[] strs) {
+        int i = 0;
+        int j = strs.length - 1;
+        while (i <= j) {
+            if (strs[j] == null) {
+                --j;
+            } else if (strs[i] != null) {
+                ++i;
+            } else {
+                strs[i] = strs[j];
+                strs[j] = null;
+                ++i;
+                --j;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
-        return new Resume[0];
+    Resume[] getAll() {  // создать новый масив пройти по старому все обьекты что не нал добавить в масив и вернуть
+        int count = 0;
+        Resume[] notNullResume = new Resume[storage.length];
+
+        for (Resume resume : storage) {
+            if( resume != null){
+                notNullResume[count] = resume;
+                count++;
+            }
+        }
+        return notNullResume;
     }
 
     int size() {
-        return 0;
+        int count = 0;
+
+        for (Resume resume : storage) {
+            if(resume != null) {
+                count++;
+            }
+        }
+        return count;
     }
+
 }
