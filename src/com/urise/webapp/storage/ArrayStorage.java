e3002185e807ab6f503f33b47d610046a564c3a2
TuliakovM <tuliakovm@gmail.com>
2021-04-16 11:28:23 +0300

    HW1

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -1,35 +1,39 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
 import java.util.Arrays;
 import java.util.Objects;
 
 /**
- * Array based storage for Resumes
+ * Array based com.urise.webapp.storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
-    int size = 0;
+    private Resume[] storage = new Resume[10000];
+    private int size = 0;
 
-    void clear() {
+    public void clear() {
         Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
-    void save(Resume r) {
+    public void save(Resume r) {
         storage[size] = r;
         size++;
     }
 
-    Resume get(String uuid) {
+    public Resume get(String uuid) {
             for (int i = 0; i < size; i++) {
-                if (uuid.equals(storage[i].uuid)) {
+                if (uuid.equals(storage[i].getUuid())) {
                     return storage[i];
                 }
             }
         return null;
     }
 
-    void delete(String uuid) {
+    public void delete(String uuid) {
             for (int i = 0; i < size; i++) {
-                if (storage[i].uuid.equals(uuid)) {
+                if (storage[i].getUuid().equals(uuid)) {
                     storage[i] = null;
                     size--;
                 }
@@ -39,9 +43,9 @@ public class ArrayStorage {
     }
 
     /**
-     * @return array, contains only Resumes in storage (without null)
+     * @return array, contains only Resumes in com.urise.webapp.storage (without null)
      */
-    Resume[] getAll() {
+    public Resume[] getAll() {
         Resume[] notNullResume = new Resume[size];
 
         for (int i = 0; i < size; i++) {
@@ -50,7 +54,7 @@ public class ArrayStorage {
         return notNullResume;
     }
 
-    int size() {
+    public int size() {
         return size;
     }
 
