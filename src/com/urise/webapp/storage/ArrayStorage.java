f3fcc0586b40a0c279821a57cec5a2ecbddf59e2
TuliakovM <tuliakovm@gmail.com>
2021-04-22 13:13:32 +0300

    update save method

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 
 
 public class ArrayStorage {
-    private Resume[] storage = new Resume[3];
+    private Resume[] storage = new Resume[10_000];
     private int size = 0;
 
     public void clear() {
